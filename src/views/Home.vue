45982f4fda5698aaab79fc2e0b1a2722eb687fb3
Andrew Signori <andrew.signori@cgi.com>
2021-04-16 14:57:27 -0700

    Equifax test

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,50 @@
 <template>
-  <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png" />
-    <HelloWorld msg="Welcome to Your Vue.js + TypeScript App" />
+  <div>
+    <span>Authorization</span>
+    <input type="text" v-model="authorization" />
+    <button @click="getToken">Get Token</button>
   </div>
 </template>
 
 <script lang="ts">
-import { defineComponent } from "vue";
-import HelloWorld from "@/components/HelloWorld.vue"; // @ is an alias to /src
+import { defineComponent, ref } from "vue";
+import axios from "axios";
+import { stringify } from "qs";
 
 export default defineComponent({
   name: "Home",
-  components: {
-    HelloWorld,
+  components: {},
+  setup() {
+    const authorization = ref("");
+
+    const getToken = async () => {
+      console.log("Testing Equifax Authentication");
+
+      const options = {
+        headers: {
+          "Content-Type": "application/x-www-form-urlencoded",
+          Authorization: authorization.value,
+        },
+      };
+
+      const data = stringify({
+        grant_type: "client_credentials", // eslint-disable-line
+        scope: "https://api.equifax.ca/inquiry/1.0/sts",
+      });
+
+      try {
+        const authResponse = await axios.post(
+          "https://api.uat.equifax.ca/v2/oauth/token",
+          data,
+          options
+        );
+        console.dir(authResponse);
+      } catch (error) {
+        console.dir(error);
+      }
+    };
+
+    return { authorization, getToken };
   },
 });
 </script>
